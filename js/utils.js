bb0ea6cca33ed7a8bf1ccd8cf014a2db1b91499b
Ivy <ouyayuyi@gmail.com>
2026-08-06 16:31:35 +0800

    Update utils.js
    更新上传备份选项

--- js/utils.js
+++ js/utils.js
@@ -527,6 +527,18 @@ async function importAllData(file) {
                 label: '真实语音配置',
                 indexedDBNeedles: ['favAudio_', '_favAudio_'],
                 localStorageNeedles: ['voiceTtsConfig']
+            },
+            {
+                id: 'moments',
+                label: '动态',
+                indexedDBNeedles: ['momentsData', 'csSpaceSettings', 'csWallpaper'],
+                localStorageNeedles: []
+            },
+            {
+                id: 'cinema',
+                label: '电影院',
+                indexedDBNeedles: ['_cinemaAppt', '_cinemaWatchlist', '_cinemaHistory', '_cinemaNego', '_cinemaPartnerInvite'],
+                localStorageNeedles: []
             }
         ];
 

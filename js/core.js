ee98dbde233fe6d442a81af30f708bfb4b1d95a4
Ivy <ouyayuyi@gmail.com>
2026-09-24 17:14:47 +0800

    Update core.js
    祝福语改名

--- js/core.js
+++ js/core.js
@@ -723,8 +723,8 @@ const LIBRARY_CONFIG = {
             { id: 'pokes', name: '拍一拍', mode: 'list' },
             { id: 'statuses', name: '对方状态', mode: 'list' },
             { id: 'surveyBank', name: '问卷题库', mode: 'list' },
-            { id: 'redpacketBank', name: '红包留言库', mode: 'list' },
             { id: 'period', name: '经期', mode: 'list' },
+            { id: 'redpacketBank', name: '红包祝福语', mode: 'list' },
             { id: 'mottos', name: '顶部格言', mode: 'list' },
             { id: 'intros', name: '开场动画', mode: 'list' }
         ]
